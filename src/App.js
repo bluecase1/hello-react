36ccec8f878297d65b22d2a457521e5963a5d89e
yongsoon <bluecase1@naver.com>
2020-01-18 00:02:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const text = 'Are you awesome?';
+    const condition = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    }
+
+    return (
+      <div class="my-div">
+        <h1>Hello, React</h1>
+        <h2>{text}</h2>
+        {condition && 'show'}
+        <div 
+          style={style}
+        // 주석이 된다?
+        // 안된다??
+        />
+
+        { /* 안나옴.... */}
+        //나옴
+        /* 역시 나옴 */
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                New Component
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
