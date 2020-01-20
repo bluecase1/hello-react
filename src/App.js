8a90c0473a04a696d3026c2c2fa964ecd14e998f
yongsoon <bluecase1@naver.com>
2020-01-20 23:44:47 +0900

    part3.Component complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
-    const text = 'Are you awesome?';
-    const condition = true;
-    const style = {
-      backgroundColor: 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) + 50,
-      width: Math.round(Math.random() * 300) + 50,
-      WebkitTransition: 'all',
-      MozTransition: 'all',
-      msTransition: 'all'
-    }
-
     return (
-      <div class="my-div">
-        <h1>Hello, React</h1>
-        <h2>{text}</h2>
-        {condition && 'show'}
-        <div 
-          style={style}
-        // 주석이 된다?
-        // 안된다??
-        />
-
-        { /* 안나옴.... */}
-        //나옴
-        /* 역시 나옴 */
-      </div>
+      <MyComponent name="React" age={4}/>
     );
   }
 }
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+    static defaultProps = {
+        name: '기본 이름'
+        
+    }
+
+    static propTypes = {
+        name: PropTypes.string,
+        age: PropTypes.number.isRequired //필수로 존재해야 하며 숫자
+    }
+
+    state = {
+        number: 0
+    }
+    
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         number: 0
+    //     }
+    // }
+
     render() {
         return (
             <div>
-                New Component
+                <p>
+                New Component<br/>
+                My name is {this.props.name}
+                </p>
+                <p>I am {this.props.age}</p>
+                <p>number : {this.state.number}</p>
+                <button onClick={()=> {
+                    this.setState({
+                        number: this.state.number+1
+                    })
+                }
+                }>Add</button>
+                
+                <button onClick={()=> {
+                    this.setState({
+                        number: 0
+                    })
+                }
+                }>Reset</button>
             </div>
         )
     }
 }
+
+//MyComponent.proptype = {
+//    name: PropTypes.string // name type is string
+//}
+
 
 export default MyComponent;
